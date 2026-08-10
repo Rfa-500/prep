0116b13845f620a5081dcf1ac2358c841e92f01d
nrandomusername82 <eddiec_5@protonmail.com>
2026-08-09 20:32:51 -0500

    Restore compact preview with visible access controls

---
+++ protected/progen-turbo.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Progen Turbo
+// @version      5.7
+// @description  Protected placeholder script
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  console.log('Progen Turbo 5.7 placeholder loaded');
+})();
